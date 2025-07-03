b1104fdb164c26046788d771062691bdd12e9f4f
IbrahimRaafattt <ibrahimRaafattt@gmail.com>
2025-07-03 18:11:14 +0200

    0.0.0.0

--- app.py
+++ app.py
@@ -122,4 +122,4 @@ def stop_chunked_download():
     return jsonify({'status': 'stopped and all chunks uploaded'})
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True,host="0.0.0.0", port=5000) 
